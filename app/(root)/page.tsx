a62366a1e666f360fd9364632bdc6d301757774b
bkmz911 <i.a.4esnokov@gmail.com>
2025-03-18 15:00:07 +0400

    Profile block in process

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,7 @@
 import type { Metadata } from "next";
 import { Headband, MainText, Games } from "@/components/shared";
-
-export const metadata: Metadata = {
-    title: "Steam App | Главная",
-    description:
-        "Service that allows you to find out the inventory value of each game from your Steam account.",
-};
+import Image from "next/image";
+import { Button } from "@/components/ui";
 
 export default function Home() {
     return (
@@ -14,6 +10,26 @@ export default function Home() {
                 <Headband />
                 <MainText />
                 <Games />
+                
+            </section>
+            <section>
+                <div className="mt-[20px] xl:mt-[50px] bg-[#2c3035] rounded-xl xl:flex xl:justify-between w-[1470px] mx-auto">
+
+                    <div className="flex justify-center items-center">
+                        <Image src="/avatar.webp" width={120} height={120} alt="avatar" className="w-[120px] h-[120px]"/>
+                        <div className="ml-5">
+                            <h2 className="text-[20px] xl:text-[26px] text-white font-semibold">Obivan Kenobi</h2>
+                            <span className="mt-[10px] xl:mt-[20px] text-[14px] xl:text-[20px] text-[#6e7174] font-semibold">Стоимость инвентаря по <span className="text-white font-semibold">CS:GO :</span></span><br/>
+                            <span className="text-[#4DAEFC] text-[20px] xl:text-[32px] font-semibold">54645 ₽ </span>
+                        </div>
+                    </div>
+
+                    <div>
+                        <p className="text-[#6e7174] text-[14px] xl:text-[20px] font-semibold text-center">Скачать стоимость инвентаря в <span className="text-white">txt формате:</span></p>
+                        <Button>Скачать</Button>
+                    </div>
+
+                </div>
             </section>
         </>
     );
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,14 +1,11 @@
-interface Props {
-    text: string;
-}
-
-export const Button: React.FC<Props> = ({ text }) => {
+import { PropsWithChildren } from "react";
+export const Button: React.FC<PropsWithChildren> = ({ children }) => {
     return (
         <button
             id="button"
             className="w-[140px] xl:w-[260px] h-[36px] xl:h-[56px] bg-[#3c73dd] rounded-lg xl:rounded-2xl text-white uppercase text-[10px] xl:text-[16px] shadow-button font-semibold"
         >
-            {text}
+            {children}
         </button>
     );
 };
