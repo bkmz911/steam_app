9ac571a8ab644fd750b937f9a789c540e26a44b8
Ilya <i.a.4esnokov@gmail.com>
2025-03-24 20:47:27 +0300

    filters functionality in process

--- components/shared/filters-block/main-filters-part.tsx
+++ components/shared/filters-block/main-filters-part.tsx
@@ -1,42 +1,62 @@
-import Image from "next/image";
-import React from "react";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 
 const filters = [
     {
-        textFilter: "Все",
+        textFilter: "Цена (без скидки)",
+        filterContent: ["По возрастанию", "По убыванию"],
     },
     {
-        textFilter: "Все",
-    },
-    {
-        textFilter: "Сетка",
-    },
-    {
-        textFilter: "Одиночный",
+        textFilter: "Цена (со скидкой)",
+        filterContent: ["По возрастанию", "По убыванию"],
     },
 ];
 
 export const MainFiltersPart = () => {
     return (
         <div className="flex items-center gap-[10px]">
-            {filters.map((filter, index) => (
-                <div
-                    className="mt-[10px] px-[15px] bg-[#2c3035] border-gray-600 border-2 rounded-md w-[165px] md:w-[235px] lg:w-[400px] xl:w-[190px] h-[30px] xl:h-[50px] mx-auto text-[13px] flex justify-center items-center"
-                    key={index}
+            {filters.map((filter) => (
+                <Menu
+                    key={filter.textFilter}
+                    as="div"
+                    className="relative inline-block text-left"
                 >
-                    <span className="text-white text-[12px] xl:text-[18px]">
-                        {filter.textFilter}
-                    </span>
-                    <img src="/arrow.svg" alt="arrow" className="ml-[5px]" />
-                </div>
+                    <div>
+                        <MenuButton className="bg-[#2c3035] inline-flex w-full justify-center gap-x-1.5 px-3 py-2 text-white text-[12px] xl:text-[18px] shadow-xs hover:bg-gray-600 rounded-[6px] border-gray-600 border-2">
+                            {filter.textFilter}
+                            <img
+                                src="/arrow.svg"
+                                alt="arrow"
+                                className="ml-[5px]"
+                            />
+                        </MenuButton>
+                    </div>
+
+                    <MenuItems
+                        transition
+                        className="absolute right-0 z-10 mt-2 w-full origin-top-right rounded-md bg-[#2c3035] border-gray-600 border-2 ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+                    >
+                        <div className="py-1">
+                            {filter.filterContent?.map((item) => (
+                                <MenuItem key={item}>
+                                    <p className="block px-4 py-2 text-sm text-white text-[12px] xl:text-[18px] data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden">
+                                        {item}
+                                    </p>
+                                </MenuItem>
+                            ))}
+                        </div>
+                    </MenuItems>
+                </Menu>
             ))}
-            <Image
+
+            {/* Фун-нал с корзиной на перспективу */}
+
+            {/* <Image
                 src="/basket.svg"
                 width={55}
                 height={53}
                 alt="basket"
                 className="w-11 h-11 xl:w-auto xl:h-auto"
-            />
+            /> */}
         </div>
     );
 };
